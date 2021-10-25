192b2ee1c7b374d96f111a7f61fa170ccf73d415
Jasmine <70559611+Jasmine582@users.noreply.github.com>
2021-10-25 14:53:18 -0500

    Updated main.js

--- js/main.js
+++ js/main.js
@@ -6,9 +6,3 @@ function myFunction () {
         x.className = "topnav";
     }
 };
-document.querySelector("#contact-me").addEventListener("click",function(){
-    document.querySelector(".popup").classList.add("active");
-  });
-  document.querySelector(".popup .close-btn").addEventListener("click",function(){
-    document.querySelector(".popup").classList.remove("active");
-  });
